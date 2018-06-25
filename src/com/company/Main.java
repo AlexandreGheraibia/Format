59c4f8859c9494fcec280e91471427605e0655b4
gheraibia <gheraibia@hotmail.com>
2018-06-25 23:17:03 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,38 @@ public class Main {
     static  int multiplyTab(int tab[]){
        return multiplyTab1(tab,tab.length-1);
     }
+	
+	 public static int min(int[] tab,int n) {
+	   
+	   if(n<tab.length) {
+		   int min= min(tab,n+1);
+		 
+		  if(tab[n]< min) {
+			  min=tab[n];
+		  }
+		  return min>tab[n]?tab[n]:min;
+	   }
+	   return tab[0];
+	  
+	  
+   }
+	
+   public static  int max(int []tab) {
+	   return tab[max(tab,0,0)];
+   }
+   public static int max(int[] tab,int n,int j) {
+	   if(n<tab.length) {
+		   j=max(tab,n+1,j);
+		  if(tab[j]< tab[n]) {
+			  return n;
+		  }
+		 
+	   }
+	   return j;
+	  
+	  
+   }
+	
     static  int  multiplyTab1(int tab[],int n){
         if(n>0) {
            return  tab[n] * multiplyTab1(tab,n - 1);
